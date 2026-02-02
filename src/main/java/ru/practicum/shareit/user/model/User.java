7d3278d78a71c928b60f6c5bf4569096668dff9f
Novikov Aleksei <3900676@gmail.com>
2026-02-02 13:44:54 +0300

    fix user function

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,5 +1,11 @@
 package ru.practicum.shareit.user.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
@@ -7,8 +13,16 @@ import lombok.Data;
  * Хранит базовую информацию о пользователе: идентификатор, имя и email.
  */
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
 	private String name;
+
+	@Column(nullable = false, length = 512)
 	private String email;
 }
