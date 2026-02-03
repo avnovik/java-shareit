62b9ff0c5b0599438b02d16a6f171089917b7d72
Novikov Aleksei <3900676@gmail.com>
2026-02-03 12:32:43 +0300

    fix comment.text @size

--- src/main/java/ru/practicum/shareit/item/dto/CommentCreateDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentCreateDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -9,5 +10,6 @@ import lombok.Data;
 @Data
 public class CommentCreateDto {
 	@NotBlank
+	@Size(max = 1024)
 	private String text;
 }
